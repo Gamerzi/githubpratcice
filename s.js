c8ebddbfbc1f9fefef99067609e069812f8844c2
Gamerzi <sastigamingshorts789@gmail.com>
2026-09-09 08:30:17 +0530

    Js practice

---
+++ s.js
@@ -0,0 +1,2 @@
+let str1 = "JavaScript is awesome"
+let nes = str1.slice(14,20);
---
+++ script.js
@@ -0,0 +1,16 @@
+try {
+    let name = "Hello world";
+    let b = 10;
+
+    if (b == 0) {
+        throw new Error("Zero error");
+    }
+    console.log(name);
+    console.log(c);
+}
+catch (error) {
+    console.log(error.message);
+}
+finally {
+    console.log("Java Script is completed");
+}
